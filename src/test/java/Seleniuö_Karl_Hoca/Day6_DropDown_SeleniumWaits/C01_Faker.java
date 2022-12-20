6a6520abc768ae9ba88a6c438909f894a54fe432
ismail5240 <arkd50279@gmail.com>
2022-12-21 00:19:00 +0300

    21,12,22

---
+++ src/test/java/Seleniuö_Karl_Hoca/Day6_DropDown_SeleniumWaits/C01_Faker.java
@@ -0,0 +1,22 @@
+package Seleniuö_Karl_Hoca.Day6_DropDown_SeleniumWaits;
+
+import com.github.javafaker.Faker;
+import org.junit.Test;
+
+public class C01_Faker {
+
+    //Faker değerler üretmek için Faker Class’ından bir obje üretir ve var olan metotları kullanırız.
+
+
+    @Test
+    public void fakerExample(){
+
+        //Faker objesini olusturuyoruz
+        Faker faker = new Faker();
+
+        System.out.println(faker.name().firstName());
+        System.out.println(faker.name().lastName());
+        System.out.println(faker.address().fullAddress());
+
+    }
+}
